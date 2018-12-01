ed238c9481eac9932fcf67c605f0ec5611b3935f
Albin Haglund <haggespel@gmail.com>
2018-12-01 08:47:31 +0100

    1.1

---
+++ LD43/Assets/Death.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+    public GameObject lik;
+    public Vector3 deathpoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            deathpoint = col.gameObject.transform.position;
+            Destroy(col.gameObject);
+            Instantiate(lik, deathpoint, Quaternion.identity);
+        }
+    }
+}
---
+++ LD43/Assets/Scripts/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public float jumpforce;
+
+    public float fallmultiplier = 2.5f;
+    public float lowJumpmultiplier = 2f;
+
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+        if(Input.GetButtonDown ("Jump") && grounded())
+        {
+            rb.velocity = Vector3.up * jumpforce;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics2D.gravity.y * (fallmultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics2D.gravity.y * (lowJumpmultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    bool grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1f);
+    }
+}
